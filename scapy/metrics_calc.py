45c19b4ce75d5d24db7babf16fab4ef36397e24f
Chen Weihang <sunny_cwh@163.com>
2019-04-20 23:36:25 +0800

    polish test code

--- scapy/metrics_calc.py
+++ scapy/metrics_calc.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import struct
-from scapy.all import *
+from __future__ import print_function
 
-htonll = lambda a:struct.unpack('!Q', struct.pack('Q', a))[0]
-ntohll = lambda a:struct.unpack('Q', struct.pack('!Q', a))[0]
+import os
+import sys
+from scapy.all import *
 
 class Metrics(object):
     def __init__(self):
         self.sum = 0
         self.square_sum = 0
         self.samples = 0
-        self.min = 0
+        self.min = sys.maxint
         self.max = 0
 
     def update(self, value):
         self.square_sum += value * value
         self.sum += value
-        slef.samples += 1
-        
+        self.samples += 1
+
         if value > self.max:
             self.max = value
         if value < self.min:
             self.min = value
 
-    def print(self):
+    def printm(self):
         average_ns = self.sum / float(self.samples)
         var_ns2 = self.square_sum / float(self.samples)
-        print("\t\tAverage: %llu ns Variance %llu ns2\n", average_ns, var_ns2)
-        print("\t\tMax: %llu ns Min:: %llu ns\n", self.max, self.min)
-        print("\t\tSamples: %llu\n", self.samples)
+        print(type(self).__name__, ":")
+        print("\tAverage:", average_ns, "us, Variance:", var_ns2, "us2")
+        print("\tMax:", self.max, "us, Min:", self.min, "us")
+        print("\tSamples:", self.samples)
 
 
 class Latency(Metrics):
@@ -41,13 +42,15 @@ class Latency(Metrics):
 
 class InterArrival(Metrics):
     def __init__(self):
+        Metrics.__init__(self)
         self.last_recv = 0
         
     def calc(self, recv_time):
         if self.last_recv == 0:
+            self.last_recv = recv_time
             return
 
-        inter_arrival = recv_time - last_recv
+        inter_arrival = recv_time - self.last_recv
         self.update(inter_arrival)
         
         self.last_recv = recv_time
@@ -55,8 +58,9 @@ class InterArrival(Metrics):
 
 class Jitter(Metrics):
     def __init__(self):
+        Metrics.__init__(self)
         self.last_latency = 0
-
+    
     def calc(self, send_time, recv_time):
         latency = recv_time - send_time
         if self.last_latency == 0:
@@ -70,17 +74,17 @@ class Jitter(Metrics):
 
 
 def get_time(packet):
-    # get recv time
-    recv_time = int(packet.time * 1000000000)
-    # get send time
-    stemp = int(packet[Raw].load[8:16].encode('Hex'), 16)
-    send_time = ntohll(stemp)
+    # get recv time, us
+    recv_time = int(packet.time * 1000000)
+    # get send time, us
+    send_time = int(packet[Raw].load[8:16].encode('Hex'), 16) / 1000
 
     return send_time, recv_time
 
 
 if __name__ == '__main__':
-    pcap_path = "/home/chenwh/Workspace/Data/test.pcap"
+    pcap_dir = "/home/chenwh/Workspace/Data"
+    pcap_path = os.path.join(pcap_dir, sys.argv[1]+".pcap")
     pcaps = rdpcap(pcap_path)
 
     latency = Latency()
@@ -88,13 +92,14 @@ if __name__ == '__main__':
     jitter = Jitter()
 
     for packet in pcaps:
-        if packet[IP].src == "10.0.0.1":
+       if packet.haslayer('IP') and packet[IP].src == "10.0.0.1":
             send_time, recv_time = get_time(packet)
+            # print("send time:", send_time, " recv time:", recv_time)
             latency.calc(send_time, recv_time)
             inter_arrival.calc(recv_time)
             jitter.calc(send_time, recv_time)
 
-    latency.print()
-    inter_arrival.print()
-    jitter.print()
+    latency.printm()
+    inter_arrival.printm()
+    jitter.printm()
 
